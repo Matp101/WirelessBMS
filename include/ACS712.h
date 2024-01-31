bd472d2ba0ecbf4de827b94890dcb66bb4578c49
Mathew Pellarin <mat_p1999@hotmail.com>
2024-01-30 21:21:17 -0500

    Updated AC712

--- include/ACS712.h
+++ include/ACS712.h
@@ -3,108 +3,96 @@
 
 #include <Arduino.h>
 
-#define ADC_SCALE 1023.0
-#define VREF 5.0
-#define DEFAULT_FREQUENCY 60
+#define ACS712_05B_SENSITIVITY 185 // mV/A
+#define ACS712_20A_SENSITIVITY 100 // mV/A
+#define ACS712_30A_SENSITIVITY 66  // mV/A
 
-enum ACS712_type
+enum ACS712Type
 {
-	ACS712_05B,
-	ACS712_20A,
-	ACS712_30A
+	ACS712_05B = 0,
+	ACS712_20A = 1,
+	ACS712_30A = 2,
+	ACS712_N = 3
 };
 
 class ACS712
 {
-private:
-	int _zero = 512;
-	float _sensitivity;
-	uint8_t _pin;
-
 public:
-	// Setup sensor, type of sensor and pin
-	// Sensitivity is mV/A for 20A and 30A module, mV/A for 5A module
-	// Type: ACS712_05B, ACS712_20A, ACS712_30A
-	ACS712(ACS712_type type, uint8_t pin)
+	ACS712(ACS712Type _type, uint8_t _pin, float _adc_ref = 5, int _adc_fs = 1023)
 	{
-		_pin = pin;
+		type = _type;
+		adc_fs = _adc_fs;
 
-		switch (type)
+		// Define sensor pin
+		ACS712_pin = _pin;
+
+		// Define sensor offset
+		offset = _adc_fs / 2.0;
+
+		// Define sensor sensitivity according to the sensor type.
+		inv_sensitivity = 1000.0 / float(sensor_sen[_type]); // V/A -> A/V
+
+		results_adjuster = _adc_ref / float(_adc_fs); // V/ADC
+		results_adjuster *= inv_sensitivity;				 // V/ADC x A/V -> A/ADC
+	}
+
+	float autoCalibrate()
+	{
+		int _adc = 0, _sample = 0;
+		while (_sample < 10)
 		{
-		case ACS712_05B:
-			_sensitivity = 185.0;
-			break;
-		case ACS712_20A:
-			_sensitivity = 100.0;
-			break;
-		case ACS712_30A:
-			_sensitivity = 66.0; 
-			break;
-		}
-	};
-
-	// Setup the sensor
-	void begin()
-	{
-		pinMode(_pin, INPUT);
-	};
-
-	// Calibrate zero value
-	int calibrate()
-	{
-		uint16_t acc = 0;
-		for (int i = 0; i < 10; i++)
-		{
-			acc += analogRead(_pin);
-		}
-		_zero = acc / 10;
-		return _zero;
-	};
-
-	// Set zero point in ADC counts
-	void setZeroPoint(int zeroPoint)
-	{
-		_zero = zeroPoint;
-	};
-
-	// Set sensitivity (mV/A)
-	void setSensitivity(float sens)
-	{
-		_sensitivity = sens;
-	};
-
-	// Get Current DC value in mA
-	float getCurrentDC()
-	{
-		int16_t acc = 0;
-		for (int i = 0; i < 10; i++)
-		{
-			acc += analogRead(_pin) - _zero;
-		}
-		float I = ((float)acc / 10.0) / ADC_SCALE * VREF / _sensitivity;
-		return I;
-	};
-
-	// Get Current for AC voltage in mA
-	float getCurrentAC(uint16_t frequency = DEFAULT_FREQUENCY)
-	{
-		uint32_t period = 1000000 / frequency;
-		uint32_t t_start = micros();
-
-		uint32_t Isum = 0, measurements_count = 0;
-		int32_t Inow;
-
-		while (micros() - t_start < period)
-		{
-			Inow = analogRead(_pin) - _zero;
-			Isum += Inow * Inow;
-			measurements_count++;
+			_adc += analogRead(ACS712_pin);
+			_sample++;
 		}
 
-		float Irms = sqrt(Isum / measurements_count) / ADC_SCALE * VREF / _sensitivity;
-		return Irms;
+		offset = float(_adc) * 0.1; // average of 10 samples
+		return offset;
+	}
+
+	float getDC(int _count = 10)
+	{
+		int _adc = 0, _sample = 0;
+		while (_sample < _count)
+		{
+			_adc += analogRead(ACS712_pin);
+			_sample++;
+		}
+
+		float _adc_avg = (float(_adc) / float(_count)) - offset; // average of 10 samples and remove offset
+		float _current = _adc_avg * results_adjuster;			 // ADC x A/ADC -> A
+		return _current;
+	}
+
+	float getOffset() { return offset; }
+
+	float getSensitivity() { return 1000.0 / inv_sensitivity; }
+
+	void setOffset(float _offset) { offset = _offset; }
+
+	void setSensitivity(float _sen)
+	{
+		results_adjuster /= inv_sensitivity; // A/ADC / A/V -> V/ADC
+		inv_sensitivity = 1000.0 / _sen;	 // V/A -> A/V
+		results_adjuster *= inv_sensitivity; // V/ADC x A/V -> A/ADC
 	};
 
+	void reset()
+	{
+		float _sen = float(sensor_sen[type]);
+		setSensitivity(_sen);
+		offset = adc_fs / 2.0;
+	};
+
+private:
+	uint8_t sensor_sen[ACS712_N] = {ACS712_05B_SENSITIVITY,
+									  ACS712_20A_SENSITIVITY,
+									  ACS712_30A_SENSITIVITY};
+	float inv_sensitivity;
+	float results_adjuster;
+	float offset;
+	int adc_fs;
+	uint8_t ACS712_pin;
+	ACS712Type type;
 };
 
 #endif
